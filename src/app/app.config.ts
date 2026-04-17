3e8d2528ee7f5601fb657dcbe2b467048331e369
Alma Qm <almaquesadamartinez@gmail.com>
2026-04-17 13:55:51 +0200

    pages, cambio idioma, interfaces, base componentes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,22 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import {provideTranslateService} from "@ngx-translate/core";
+import {provideTranslateHttpLoader} from "@ngx-translate/http-loader";
+import {provideHttpClient} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/assets/i18n/',
+        suffix: '.json'
+      }),
+      fallbackLang: 'es',
+      lang: 'es'
+    })
+  ],
 };
--- src/app/pages/plantas/plantas.ts
+++ src/app/pages/plantas/plantas.ts
@@ -1,9 +1,46 @@
+import { Injectable, signal } from '@angular/core';
+import { Planta } from '../../models/interfaces';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranlationDirective } from '@ngx-translate/core;
 
 @Component({
   selector: 'app-plantas',
-  imports: [],
+  imports: [ReactiveFormsModule, TranlationDirective],
   templateUrl: './plantas.html',
   styleUrl: './plantas.scss',
 })
-export class Plantas {}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PlantasService { // Recomendado añadir "Service" al nombre
+
+  // Usamos el tipo Planta[] en lugar de any para tener autocompletado
+  private plantasSignal = signal<Planta[]>([]);
+  
+  // Exponemos la signal de solo lectura para los componentes
+  public readonly plantas = this.plantasSignal.asReadonly();
+
+  constructor() {
+    this.loadMockData(); // Llamamos a una función inicial
+  }
+
+  private loadMockData() {
+    const mock: Planta[] = [
+      {
+        nombre: 'Tomate Cherry',
+        fecha_inicio: new Date(),
+        fecha_siembra: new Date(),
+        clima_ideal: 'Cálido',
+        imagen: 'assets/tomate.jpg',
+        estado: 'creciendo',
+        tipo: 'exterior',
+        notificaciones: true
+      }
+    ];
+    this.plantasSignal.set(mock);
+  }
+
+  // Aquí irán vuestros métodos CRUD (add, delete, update)
+}
