9c66ca8af7bab0409d97e3f1852041a3402df217
Carmee <dev.mee96@gmail.com>
2026-05-07 14:55:09 +0200

    feat: register all page routes for navbar navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,32 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'recetas',
+    redirectTo: 'inicio',
     pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    loadComponent: () =>
+      import('./pages/home/home').then(m => m.Home)
+  },
+  {
+    path: 'plantas',
+    loadComponent: () =>
+      import('./pages/plantas/plantas').then(m => m.PlantasComponent)
   },
   {
     path: 'recetas',
     loadComponent: () =>
       import('./pages/recetas/recetas').then(m => m.RecetasComponent)
+  },
+  {
+    path: 'comunidad',
+    loadComponent: () =>
+      import('./pages/comunidad/comunidad').then(m => m.Comunidad)
+  },
+  {
+    path: 'perfil',
+    loadComponent: () =>
+      import('./pages/perfil/perfil').then(m => m.Perfil)
   }
 ];
